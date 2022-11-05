9ea40a70f15bec160410951a9984d28cc1315364
Tran, Khanh <trank@oclc.org>
2022-11-05 10:50:12 -0400

    Remove .devcontainer, update truffle version

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
+      host: "ganache-cli",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
     docker: {
